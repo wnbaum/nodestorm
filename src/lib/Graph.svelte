718083fe11fbdf8ade99b522ab59658606abb8ee
wnbaum <wnbaum04@gmail.com>
2023-07-18 16:38:08 -0400

    graph

--- src/lib/Graph.svelte
+++ src/lib/Graph.svelte
@@ -1,21 +1,105 @@
 <script lang="ts">
-	import type { Vec } from "./utils.js";
+	import { Vec } from "./utils.js";
 	import { onMount } from "svelte";
+	import { cameraToOffset, offsetToCamera, worldToCamera, cameraToWorld } from "./utils.js";
 
 	export let width: number = 600;
 	export let height: number = 400;
 
 	let main: HTMLElement;
+	let transform: HTMLElement;
 
-	let cameraPosition: Vec;
-	let zoom: number;
+	let cameraPos: Vec = new Vec(0, 0);
+	let mousePos: Vec;
+	let zoom: number = 1;
+	let prevZoom: number = 1;
 
 	onMount(() => {
 		main.addEventListener("contextmenu", (e) => e.preventDefault());
+		main.addEventListener("mousedown", mouseDown)
+		main.addEventListener("mousemove", mouseMove)
+		main.addEventListener("mouseup", mouseUp)
+		main.addEventListener("mousewheel", mouseWheel)
+
+		updateGraph()
 	});
+
+	// Dragging #region
+
+	let dragging: boolean;
+	let dragDown: Vec;
+	let dragDownCameraPos: Vec;
+
+	function mouseDown(e: MouseEvent) {
+		if (e.button == 2 && e.target == main) {
+			dragDown = new Vec(e.offsetX, e.offsetY);
+			dragDownCameraPos = cameraPos.copy();
+			dragging = true;
+		}
+	}
+	
+	function mouseMove(e: MouseEvent) {
+		mousePos = new Vec(e.offsetX, e.offsetY);
+
+		if (dragging) {
+			cameraPos = dragDownCameraPos.subtract(mousePos.subtract(dragDown).div(zoom));
+			updateGraph()
+		}
+	}
+
+	function mouseUp(e: MouseEvent) {
+		dragging = false;
+	}
+
+	function mouseWheel(e: WheelEvent) {
+		if (!dragging) {
+			zoom *= 1 - e.deltaY*0.001;
+			updateGraph();
+		}
+	}
+
+	function updateGraph() {
+		if (zoom != prevZoom) {
+			let ratio = zoom/prevZoom - 1;
+			cameraPos = cameraPos.add(offsetToCamera(mousePos, main).scale(ratio).div(zoom));
+		}
+
+		prevZoom = zoom;
+
+		let bgPos: Vec = cameraToOffset(worldToCamera(new Vec(0, 0), cameraPos, zoom), main);
+
+		main.style.setProperty("background-position", `${bgPos.x}px ${bgPos.y}px`);
+		main.style.setProperty("background-size", `${40*zoom}px ${40*zoom}px`);
+
+		transform.style.setProperty("transform", `translate(${bgPos.x}px, ${bgPos.y}px) scale(${zoom})`)
+	}
+
+	// #endregion
 </script>
 
-<main bind:this={main} style="width: {width}px; height: {height}px; background: #444;" />
+<main bind:this={main} class="main" style="width: {width}px; height: {height}px;">
+	<div bind:this={transform} class="transform">
+		<div style="width: 100px; height: 100px; background: red;"/>
+	</div>
+</main>
 
 <style>
+	.main {
+		background: #444;
+		background-image: 
+			linear-gradient(to right, #ccc 1px, transparent 1px), 
+			linear-gradient(to bottom, #ccc 1px, transparent 1px);
+
+		overflow: hidden;
+
+		position: relative;
+	}
+
+	.transform {
+		width: 0px;
+		height: 0px;
+		position: absolute;
+		left: 0;
+		top: 0;
+	}
 </style>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,5 +7,41 @@ export class Vec {
 		this.y = y;
 	}
 
+	copy(): Vec {
+		return new Vec(this.x, this.y)
+	}
 
+	add(v: Vec): Vec {
+		return new Vec(this.x + v.x, this.y + v.y);
+	}
+
+	subtract(v: Vec): Vec {
+		return new Vec(this.x - v.x, this.y - v.y);
+	}
+
+	scale(a: number): Vec {
+		return new Vec(this.x * a, this.y * a);
+	}
+
+	div(a: number): Vec {
+		return new Vec(this.x / a, this.y / a);
+	}
+}
+
+export function worldToCamera(pos: Vec, cameraPos: Vec, zoom: number): Vec {
+	return pos.subtract(cameraPos).scale(zoom)
+}
+
+export function cameraToWorld(pos: Vec, cameraPos: Vec, zoom: number): Vec {
+	return pos.div(zoom).add(cameraPos)
+}
+
+export function offsetToCamera(pos: Vec, main: Element): Vec {
+	const rect: DOMRect = main.getBoundingClientRect()
+	return pos.subtract(new Vec(rect.width * 0.5, rect.height * 0.5));
+}
+
+export function cameraToOffset(pos: Vec, main: Element): Vec {
+	const rect: DOMRect = main.getBoundingClientRect()
+	return pos.add(new Vec(rect.width * 0.5, rect.height * 0.5))
 }
